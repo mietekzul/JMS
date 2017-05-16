05f24ac9dcf4af9ae9bfdf44ce4ba0560884756e
dlok <Diabolo198>
2017-05-16 08:34:24 +0200

    Added Receiver and refactored structure.

--- JMS2/src/main/java/pl/raziel/jms2/JMS2Sender.java
+++ JMS2/src/main/java/pl/raziel/jms2/JMS2Sender.java
@@ -5,8 +5,8 @@ import javax.jms.JMSContext;
 import javax.jms.Queue;
 
 public class JMS2Sender {
-	public static void main(String[] args) {
 
+	public static void main(String[] args) {
 		ConnectionFactory cf = new com.sun.messaging.ConnectionFactory();
 		try (JMSContext jmsContext = cf.createContext()) {
 			Queue queue = jmsContext.createQueue("EM_JMS2_TRADE.Q");
