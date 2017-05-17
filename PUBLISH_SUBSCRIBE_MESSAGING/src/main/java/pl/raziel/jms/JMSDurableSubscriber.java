d30cdfa2a2073fdac6af79a62c5da0ec5ff19cb1
mietekzul <noname_18@wp.pl>
2017-05-17 23:12:20 +0200

    Added durable subscriber.

--- PUBLISH_SUBSCRIBE_MESSAGING/src/main/java/pl/raziel/jms/JMSDurableSubscriber.java
+++ PUBLISH_SUBSCRIBE_MESSAGING/src/main/java/pl/raziel/jms/JMSDurableSubscriber.java
@@ -12,22 +12,22 @@ import javax.jms.TopicConnection;
 import javax.jms.TopicSession;
 import javax.jms.TopicSubscriber;
 
-public class JMSSubscriber implements MessageListener {
+public class JMSDurableSubscriber implements MessageListener {
+
 	public static void main(String[] args) {
-		new Thread(() -> new JMSSubscriber()).start();
+		new Thread(() -> new JMSDurableSubscriber()).start();
 	}
 
-	public JMSSubscriber() {
+	public JMSDurableSubscriber() {
 		try {
 			TopicConnection connection =
-					new ActiveMQConnectionFactory("tcp://localhost:61616").createTopicConnection();
+					new ActiveMQConnectionFactory("tcp://localhost:61616?jms.clientID=client:123").createTopicConnection();
 			connection.start();
 			TopicSession session = connection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
 			Topic topic = session.createTopic("EM_TRADE.T");
-
-			TopicSubscriber subscriber = session.createSubscriber(topic);
+			TopicSubscriber subscriber = session.createDurableSubscriber(topic, "sub:456");
 			subscriber.setMessageListener(this);
-			System.out.println("Waiting for messages... ");
+			System.out.println("Waiting for messages...");
 		} catch (JMSException e) {
 			e.printStackTrace();
 		}
@@ -41,6 +41,5 @@ public class JMSSubscriber implements MessageListener {
 		} catch (JMSException e) {
 			e.printStackTrace();
 		}
-
 	}
 }
